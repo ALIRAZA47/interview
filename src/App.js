e28e6842246e5a88434cff856da932727fdf1e84
ALIRAZA47 <raza2018@namal.edu.pk>
2022-03-05 12:50:03 +0500

    Add Functionality Added (Partially)

--- src/App.js
+++ src/App.js
@@ -6,17 +6,40 @@ import Card from "./Card";
 
 const CardContext = React.createContext();
 
+// Add new card func
+
 function App() {
-  const [data, setData] = useState(Data);
+  var [data, setData] = useState(Data);
+  const addNewCard = () => {
+    setData([
+      ...data,
+      {
+        title: "New Card",
+        pColor: "grey",
+        bColor: ["#263238", "#90A4AE", "#616161"],
+      },
+    ]);
+  };
   console.log(Data);
   return (
-    <CardContext.Provider value={setData}>
-      <div className="cards">
-        {data.map((card) => {
-          return <Card key={card.title} {...card} />;
-        })}
-      </div>
-    </CardContext.Provider>
+    <div className="cards">
+      {data.map((card) => {
+        return <Card key={card.title} {...card} />;
+      })}
+      <button
+        className="card"
+        style={{
+          fontSize: "50px",
+          backgroundColor: "#e4dce5",
+          borderColor: "white",
+        }}
+        onClick={() => {
+          addNewCard();
+        }}
+      >
+        <b>+</b>
+      </button>
+    </div>
   );
 }
 
